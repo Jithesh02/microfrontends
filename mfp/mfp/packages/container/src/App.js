755ba754d56bef6122f62583184f68a6aa2463f7
soppa jithesh <71004729+Jithesh02@users.noreply.github.com>
2023-07-03 14:20:10 +0530

    Update App.js

--- mfp/mfp/packages/container/src/App.js
+++ mfp/mfp/packages/container/src/App.js
@@ -2,5 +2,7 @@ import React from "react";
 import Marketing from './components/Marketing';
 
 export default ()=>{
-    return <div><h1>Hiifiii!</h1><Marketing /></div>
+    return <div><h1>Hiifiii!</h1>
+        // <Marketing />
+        </div>
 };
