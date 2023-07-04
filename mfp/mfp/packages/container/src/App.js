4a35b76b8fc1e50eb7011d201995a2f43c2eb9b8
jithesh02 <jitheshsoppa2002@gmail.com>
2023-07-04 21:50:29 +0530

    1234

--- mfp/mfp/packages/container/src/App.js
+++ mfp/mfp/packages/container/src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import Marketing from './components/Marketing';
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
+import { StylesProvider,createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
 
 export default ()=>{
     return (
         <BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
           <div>
               <Header />
               <Marketing />
           </div>
+          </StylesProvider>
         </BrowserRouter>
     );
 };
